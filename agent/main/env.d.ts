96ee4a9c0778e83e64fec4c40bf78ad83f39a051
CI <staff@ulixee.org>
2024-05-08 08:35:26 +0000

    Auto-build Javascript files

---
+++ agent/main/env.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    disableMitm: boolean;
+    showChrome: boolean;
+    noChromeSandbox: boolean;
+    useRemoteDebuggingPort: boolean;
+    disableGpu: boolean;
+    enableHeadlessNewMode: boolean;
+    defaultChromeId: string;
+    useRosettaChromeOnMac: boolean;
+};
+export default _default;
---
+++ plugins/default-browser-emulator/injected-scripts/console.js
@@ -0,0 +1,24 @@
+ObjectCached.keys(console).forEach(key => {
+    proxyFunction(console, key, (target, thisArg, args) => {
+        args = replaceErrorStackWithOriginal(args);
+        return ReflectCached.apply(target, thisArg, args);
+    });
+});
+const defaultErrorStackGetter = Object.getOwnPropertyDescriptor(new Error(''), 'stack').get;
+function replaceErrorStackWithOriginal(object) {
+    if (!object || typeof object !== 'object') {
+        return object;
+    }
+    if (object instanceof Error) {
+        if (ObjectCached.getOwnPropertyDescriptor(object, 'stack')?.get === defaultErrorStackGetter) {
+            return object;
+        }
+        const error = new Error(`Unblocked stealth created new error from: ${JSON.stringify(object)}`);
+        error.stack = `${error.message}\n   Stack removed to prevent leaking debugger active (error stack was proxied)`;
+        return error;
+    }
+    if (object instanceof Array) {
+        return object.map(item => replaceErrorStackWithOriginal(item));
+    }
+    return ObjectCached.values(object).map(item => replaceErrorStackWithOriginal(item));
+}
